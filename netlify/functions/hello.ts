e81dbecb51ce50160698c037651021a460267d29
nikilr7 <nikilraja11@gmail.com>
2026-04-01 12:37:09 +0530

    overall set-up are completed Successfully

--- netlify/functions/hello.ts
+++ netlify/functions/hello.ts
@@ -1,6 +1,22 @@
+import client from "./db";
+
 export const handler = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Backend working 🚀" }),
-  };
+  try {
+    const res = await client.query("SELECT NOW()");
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "DB connected 🚀",
+        time: res.rows[0],
+      }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: "DB connection failed",
+      }),
+    };
+  }
 };
